64f32dd1286d00c70f7a48351a1ea6151b7cd507
Dungha125 <dungha122405@gmail.com>
2024-07-30 05:08:52 +0700

    dungha

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar: React.FC<Props> = ({ linkto }) => {
     };
 
     return (
-        <div className='w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10'>
+        <div className='w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#020213fb] md:bg-[#03001417] md:backdrop-blur-md z-50 px-10'>
             <div className='w-full h-full flex flex-row items-center justify-between m-auto px-[10px]'>
                 <a href={`#${linkto}`} className='h-full w-auto flex flex-row items-center'>
                     <Image
