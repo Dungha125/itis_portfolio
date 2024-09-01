e3e2d96d95aca45262378d89971572205f1edf79
Dungha125 <dungha122405@gmail.com>
2024-09-01 15:19:20 +0700

    dungha

--- scripts/obfuscate.js
+++ scripts/obfuscate.js
@@ -2,18 +2,14 @@ const fs = require('fs');
 const path = require('path');
 const JavaScriptObfuscator = require('javascript-obfuscator');
 
-const directories = [
-  path.join(__dirname, '../.next/static'),
-  path.join(__dirname, '../components'), 
-  path.join(__dirname, '../app'),
-];
+const buildPath = path.join(__dirname, '../.next/static');
 
 const obfuscateFiles = (dir) => {
   fs.readdirSync(dir).forEach(file => {
     const fullPath = path.join(dir, file);
     if (fs.lstatSync(fullPath).isDirectory()) {
       obfuscateFiles(fullPath);
-    } else if (file.endsWith('.js') || file.endsWith('.tsx')) {
+    } else if (file.endsWith('.js')) { 
       const code = fs.readFileSync(fullPath, 'utf8');
       const obfuscatedCode = JavaScriptObfuscator.obfuscate(code, {
         compact: true,
@@ -24,10 +20,8 @@ const obfuscateFiles = (dir) => {
   });
 };
 
-directories.forEach(dir => {
-  if (fs.existsSync(dir)) {
-    obfuscateFiles(dir);
-  } else {
-    console.log(`Build path not found: ${dir}`);
-  }
-});
+if (fs.existsSync(buildPath)) {
+  obfuscateFiles(buildPath);
+} else {
+  console.log(`Build path not found: ${buildPath}`);
+}
