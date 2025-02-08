6c54c0852a315980862984632d5b618335ffb8e0
Dungha125 <dungha122405@gmail.com>
2025-02-08 16:55:36 +0700

    fix main

--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -3,17 +3,21 @@ import MainContent from '../content/MainContent';
 
 const Main = () => {
   return (
-    <div className='relative flex flex-col md:flex-row items-center justify-center w-full min-h-screen bg-white lg:px-20 xl:px-32 2xl:px-40'>
-      <MainContent /> 
-      <div className="w-full md:w-1/2 flex justify-center items-center h-full">
-        <img 
-          src="./SVG/icon.svg" 
-          alt="icon" 
-          className="w-full max-w-xs md:max-w-md lg:max-w-lg h-auto" 
-        />
-      </div>
+    // <div className='relative flex flex-col md:flex-row items-center justify-center w-full min-h-screen bg-white lg:px-20 xl:px-32 2xl:px-40'>
+    //   {/* <MainContent /> 
+    //   <div className="w-full md:w-1/2 flex justify-center items-center h-full">
+    //     <img 
+    //       src="./SVG/icon.svg" 
+    //       alt="icon" 
+    //       className="w-full max-w-xs md:max-w-md lg:max-w-lg h-auto" 
+    //     />
+    //   </div> */}
+      
        
-    </div>
+    // </div>
+    <div className='w-full h-full'>
+      <img src="/itiscup/cover-01.svg" alt="Cover" className='object-cover w-full mt-[65px]' />
+   </div>
   );
 };
 
